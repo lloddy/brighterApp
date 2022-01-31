b0adc509c6301a954fc33e22bc0269e0a8a668c1
lloddy <lloddy@gmail.com>
2022-01-31 16:58:00 -0500

    WIP creates a single-post page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import NavBar from "./components/navbar/NavBar";
 import Home from './pages/home/Home'
+import Single from "./pages/single/Single";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Home />
+      <Single/>
     </div>
   );
 }
---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,46 @@
+import './singlePost.css'
+
+const SinglePost = (props) => {
+    return (
+        <div className="singlePost">
+            <div className="singlePostWrapper">
+                <img 
+                    src="https://images.unsplash.com/photo-1492052722242-2554d0e99e3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80" 
+                    alt="postimage" 
+                    className="singlePostImage" />
+                <h1 className="singlePostTitle">
+                    Lorem ipsum dolor sit
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon fas fa-edit"></i>
+                        <i className="singlePostIcon far fa-trash-alt"></i>
+                    </div>
+                </h1>
+                <div className="singlePostInfo">
+                    <span className='singlePostAuthor'>Author: <b>Lloyd</b></span>
+                    <span className='singlePostDate'>1 hour ago</span>
+                </div>
+                    <p>Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. 
+                        Aliquam ex sunt eveniet id distinctio 
+                        numquam, praesentium dolores cumque omnis 
+                        corporis iste commodi deserunt nulla est 
+                        harum ratione incidunt dolor! Sunt?
+                        Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. 
+                        Aliquam ex sunt eveniet id distinctio 
+                        numquam, praesentium dolores cumque omnis 
+                        corporis iste commodi deserunt nulla est 
+                        harum ratione incidunt dolor! Sunt?
+                        Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. 
+                        Aliquam ex sunt eveniet id distinctio 
+                        numquam, praesentium dolores cumque omnis 
+                        corporis iste commodi deserunt nulla est 
+                        harum ratione incidunt dolor! Sunt?</p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default SinglePost
