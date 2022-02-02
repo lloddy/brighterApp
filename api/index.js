a50891ac9c8b3a0f56e1a3cbabcb3bcd818de1ce
lloddy <lloddy@gmail.com>
2022-02-02 16:30:18 -0500

    WIP adds photo upload

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
 const categoryRoute = require("./routes/categories")
+const multer = require("multer")
 
 dotenv.config();
 app.use(express.json());
@@ -16,6 +17,19 @@ mongoose.connection
     .on("open", () => console.log("Congrats, you're connected to MongoDB!"))
     .on("close", () => console.log("You are now disconnected from MongoDB"))
     .on("error", (error) => console.log(error));
+
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, "images")
+    }, filename:(req, file, cb) => {
+        cb(null,req.body.name);
+    },
+});
+
+const upload = multer({storage: storage})
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded")
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
