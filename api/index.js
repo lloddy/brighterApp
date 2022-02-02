eb28cf0128bc57e4d9e2c9b19d9254805916d887
lloddy <lloddy@gmail.com>
2022-02-02 13:40:01 -0500

    WIP connects app to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
-const app = express()
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
 
-app.listen("5000", () => {
+dotenv.config();
+const { PORT, DATABASE_URL} = process.env;
+
+mongoose.connect(DATABASE_URL);
+mongoose.connection
+    .on("open", () => console.log("Congrats, you're connected to MongoDB!"))
+    .on("close", () => console.log("You are now disconnected from MongoDB"))
+    .on("error", (error) => console.log(error));
+
+
+app.listen(PORT, () => {
     console.log("backend is running")
 })
