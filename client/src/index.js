2f1cf8e0bdfee4a6552b9211f6e4f2c115a4f401
lloddy <lloddy@gmail.com>
2022-02-03 17:37:30 -0500

    WIP adds start of login with context

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
+import { ContextProvider } from './context/Context';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </Router>,
   document.getElementById('root')
 );
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,20 +1,36 @@
 import './login.css';
 import { Link } from 'react-router-dom';
+import { useRef } from 'react';
 
 const Login = (props) => {
+    const userRef = useRef()
+    const passwordRef = useRef()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
         <div className="login">
             <span className="loginTitle">Login</span>
-            <form className="loginForm">
-                <label>Email</label>
-                <input type="text" className='loginInput' placeholder='Enter your email'/>
+            <form className="loginForm" onSubmit={handleSubmit}>
+                <label>Username</label>
+                <input 
+                    type="text" 
+                    className='loginInput' 
+                    placeholder='Enter your Username' 
+                    ref={userRef}
+                />
                 <label>Password</label>
-                <input type="password" className='loginInput' placeholder='Enter your password'/>
-                <button className="loginButton">Login</button>
+                <input 
+                    type="password" 
+                    className='loginInput' 
+                    placeholder='Enter your password'
+                    ref={passwordRef}
+                />
+                <button className="loginButton" type="submit">Login</button>
+            </form>
                 <button className="loginRegisterButton">
                     <Link to='/register' className='link'>Create an Account</Link> 
                 </button>
-            </form>
         </div>
     )
 }
