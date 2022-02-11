0d22f7277072d2a8dc1a7ef502a8530983ec5f3b
lloddy <lloddy@gmail.com>
2022-02-11 17:28:50 -0500

    WIP adds basic resource and about page set-up

---
+++ client/src/pages/about/About.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../../components/sidebar copy/Sidebar"
+
+const About = (props) => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default About
---
+++ client/src/pages/resources/Resources.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../../components/sidebar copy/Sidebar"
+
+const Resources = (props) => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Resources
